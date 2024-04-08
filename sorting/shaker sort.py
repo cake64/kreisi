4784015c97507535313be7e2ef67c200a3ea4fe8
cake64 <166376575+cake64@users.noreply.github.com>
2024-04-08 16:41:51 +0300

    Create shaker sort.py

---
+++ sorting/shaker sort.py
@@ -0,0 +1,34 @@
+"""
+shaker sort on python
+"""
+def shaker_sort(h):
+    left = 0
+    right = len(h) - 1
+
+    while left <= right:
+        for i in range(left, right):
+            if h[i] > h[i + 1]:
+                h[i], h[i + 1] = h[i + 1], h[i]
+        right -= 1
+
+        for i in range(right, left, -1):
+            if h[i - 1] > h[i]:
+                h[i], h[i - 1] = h[i - 1], h[i]
+        left += 1
+
+
+def main():
+    import random
+    random.seed("yeah ok bro")
+    a = list(range(50000))
+    random.shuffle(a)
+
+    # sort it
+    shaker_sort(a)
+
+    # print it
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
