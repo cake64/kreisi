80605d687345cffc037cb4226f855b0db6d1fb70
cake64 <166376575+cake64@users.noreply.github.com>
2024-04-08 17:33:31 +0300

    Create bogosort.py

---
+++ sorting/the least logic sorting algorythms known to man/bogosort.py
@@ -0,0 +1,28 @@
+"""
+bogo sort on python
+"""
+import random
+ 
+def bogo_sort(h):
+    rng = range(0, len(h)-1) # not random number generator
+    while any((h[i] > h[i+1]) for i in rng): # check is list sorted
+        random.shuffle(h)
+ 
+
+
+def main():
+    # randomize list
+    random.seed("yeah ok bro")
+    a = list(range(10))
+    random.shuffle(a)
+
+    # sort it
+    random.seed(None)
+    bogo_sort(a)
+
+    # print it
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
