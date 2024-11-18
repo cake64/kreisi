84c4ad61bd48bec9dc5ef05b1f0b5cfbb363025b
i eat sand <xtema5002x@gmail.com>
2024-11-18 11:08:48 +0300

    g

---
+++ acmp/11-20/14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <numeric>
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << std::lcm(a, b) << '\n';
+    return 0;
+}
+
+/*
+int main() {
+    int a, b, aa, bb;
+    std::cin >> a >> b;
+    aa = a; bb = b;
+    while (aa != bb) {
+        if (aa < bb) aa += a;
+        else bb += b;
+    }
+    std::cout << aa;
+
+    return 0;
+}
+*/
