7cce38730f909190a449a042cfc282d8ee6cdd88
tema5002 <152618947+tema5002@users.noreply.github.com>
2024-11-19 19:10:32 +0300

    Create 149.cpp

---
+++ acmp/141-150/149.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n;
+    std::cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        std::cout << a[i] << " ";
+    }
+    return 0;
+}
